7faabaebd28aaeae3b8d437ae07a4374f900f574
Will Powell <87877431+wpowell31@users.noreply.github.com>
2023-03-07 16:57:47 +0000

    adding testing (#10)
    * adding testing
    
    * running black
    
    * adding requested changes
    
    * passing style checks

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # ehr-utils
 
 The ehr-utils library provides some simple analytical capabilities for EHR data.
+
+# End User Documentation
+
+## Setup/Installation instructions
+
+To use this software, you will need to have Python 3.6 or later installed on your system. You can download Python from the official website: https://www.python.org/downloads/
+
+## Input File Formats
+The module accepts data in the form of .txt files. The data are in the form of
+    Table 1: Patient Demographic Data    
+    Table 2: Labratory Results
+
+All input file should be tab sebarated, with string values not in quotes. For the Patient Demographic Data file, every entry is for a unique patient. For the Labratory Results file, the same patient can have multiple labs, and multiple labs for the same admission. The first line for both files should be a header that gives the column names for the fields in the file. An arbitrary number of headers can be supported, however typical column names for the patient demographic file are PatientID, PatientGender, PatientDateOfBirth, PatientRace, PatientMaritalStatus, PatientLanguage, and PatientPopulationPercentageBelowPoverty. For the labratory results file the typical headers are PatientID, AdmissionID, LabName, LabValue, LabUnits, and LabDateTime.
+
+## API
+Old patients
+The function patient_age(records: DATA_TYPE, patient_id: str, age: float) -> int takes the data and returns the age in years of the given patient. For example,
+
+>> patient_age(records, "1A8791E3-A61C-455A-8DEE-763EB90C9B2C")
+49
+
+
+Sick patients
+The function patient_is_sick(records: DATA_TYPE, patient_id: str, lab_name: str, operator: str, value: float) takes the data and returns a boolean indicating whether the patient has ever had a test with value above (">") or below ("<") the given level. For example,
+
+>> patient_is_sick(records, "1A8791E3-A61C-455A-8DEE-763EB90C9B2C", "METABOLIC: ALBUMIN", ">", 4.0)
+True
+
+# Contributor Instructions
+
+To test the data, run sets using the pytest library. To do so, navigate to the working directory and run pytest.
+To test for coverage, run pytest --cov=ehr_module tests/. 
+
+
---
+++ tests/test_ehr_module.py
@@ -0,0 +1,123 @@
+"""Testing the EHR module."""
+from ehr_module import parse_data
+from ehr_module import patient_is_sick
+from ehr_module import patient_age
+from fake_files import fake_files
+
+
+def test_parse_data() -> None:
+    """Test the parse data function."""
+    fake_patient = [
+        [
+            "PatientID",
+            "PatientGender",
+            "PatientDateOfBirth",
+            "PatientRace",
+            "PatientMaritalStatus",
+            "PatientLanguage",
+            "PatientPopulationPercentageBelowPoverty",
+        ],
+        [
+            "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F",
+            "Male",
+            "1947-12-28 02:45:40.547",
+            "Unknown",
+            "Married",
+            "Icelandic",
+            "18.08",
+        ],
+    ]
+
+    fake_patient_result = {
+        "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F": {
+            "PatientGender": "Male",
+            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
+            "PatientRace": "Unknown",
+            "PatientMaritalStatus": "Married",
+            "PatientLanguage": "Icelandic",
+            "PatientPopulationPercentageBelowPoverty": "18.08",
+        }
+    }
+
+    fake_lab = [
+        [
+            "PatientID",
+            "AdmissionID",
+            "LabName",
+            "LabValue",
+            "LabUnits",
+            "LabDateTime",
+        ],
+        [
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+            "1",
+            "URINALYSIS: RED BLOOD CELLS",
+            "1.8",
+            "rbc/hpf",
+            "1992-07-01 01:36:17.910",
+        ],
+    ]
+
+    fake_lab_result = {
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": [
+            {
+                "AdmissionID": "1",
+                "LabName": "URINALYSIS: RED BLOOD CELLS",
+                "LabValue": "1.8",
+                "LabUnits": "rbc/hpf",
+                "LabDateTime": "1992-07-01 01:36:17.910",
+            }
+        ]
+    }
+
+    with fake_files(fake_patient, fake_lab) as filenames:
+        patient_records, lab_records = parse_data(filenames[0], filenames[1])
+        assert patient_records == fake_patient_result
+        assert lab_records == fake_lab_result
+
+
+def test_patient_age() -> None:
+    """Test patient age."""
+    patient_records = {
+        "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F": {
+            "PatientGender": "Male",
+            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
+            "PatientRace": "Unknown",
+            "PatientMaritalStatus": "Married",
+            "PatientLanguage": "Icelandic",
+            "PatientPopulationPercentageBelowPoverty": "18.08",
+        }
+    }
+    assert (
+        patient_age(patient_records, "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F")
+        == 75
+    )
+
+
+def test_patient_is_sick() -> None:
+    """Test patient_is_sick."""
+    fake_lab_records = {
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": [
+            {
+                "AdmissionID": "1",
+                "LabName": "URINALYSIS: RED BLOOD CELLS",
+                "LabValue": "1.8",
+                "LabUnits": "rbc/hpf",
+                "LabDateTime": "1992-07-01 01:36:17.910",
+            }
+        ]
+    }
+    assert patient_is_sick(
+        fake_lab_records,
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+        "URINALYSIS: RED BLOOD CELLS",
+        "<",
+        4.0,
+    )
+    assert not patient_is_sick(
+        fake_lab_records,
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+        "URINALYSIS: RED BLOOD CELLS",
+        ">",
+        4.0,
+    )
